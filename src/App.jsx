1043ad7e82e96c1815c4eaf6944a58cab6f6c96c
dveininnovationsprojects-cpu <dveininnovations.project@gmail.com>
2026-04-09 11:54:35 +0530

    updated navbar icons and UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 
 function App() {
-  return <About />;
+  return (
+    <Router>
+      <Routes>
+        {/* Default path and About path both pointing to About for now */}
+        <Route path="/" element={<About />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
